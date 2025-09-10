3db8bc3faaebaf7ca08dd418ec4acf33033f80f7
Md Ashik Mia <ashikdeone908@gmail.com>
2025-09-11 03:15:01 +0600

    Update: ReadingFillBlanks component

--- src/pages/Reading/components/ReadingFillBlanks.tsx
+++ src/pages/Reading/components/ReadingFillBlanks.tsx
@@ -1,6 +1,9 @@
 "use client";
-import { Button } from "@/components/ui/button";
+import Check from "@/components/common/Check";
+import Controllers from "@/components/common/Controllers";
+import Hint from "@/components/common/Hint";
 import { useState } from "react";
+import type { Summary } from "./ReadingMultipleChoice";
 
 interface FillBlanksProps {
   question: string;
@@ -16,22 +19,60 @@ export default function ReadingFillBlanks({
   hint,
 }: FillBlanksProps) {
   const [userAnswer, setUserAnswer] = useState("");
-  const [feedback, setFeedback] = useState("");
   const [showHint, setShowHint] = useState(false);
-  const [showSolution, setShowSolution] = useState(false);
+  // const [showSolution, setShowSolution] = useState(false);
 
-  const checkAnswer = () => {
+  // const [answers, setAnswers] = useState<{ [id: number]: string[] }>({})
+    const [status, setStatus] = useState<"match" | "wrong" | "">("")
+  //   const [showSolution, setShowSolution] = useState(false)
+  //   const [wrongAnswers, setWrongAnswers] = useState<{ [id: number]: string[] }>({})
+  //   const [correctAnswers, setCorrectAnswers] = useState<{ [id: number]: string[] }>({})
+  //   const [showHint, setShowHint] = useState(false)
+
+
+
+  const handleCheck = () => {
     if (userAnswer.trim().toLowerCase() === correctAnswer.toLowerCase()) {
-      setFeedback("✅ Correct!");
+
+      setStatus("match")
     } else {
-      setFeedback("❌ Try again.");
+      setStatus("wrong")
     }
   };
+
+  const handleShowSolution = () => {
+    setUserAnswer(correctAnswer);
+    setStatus("")
+  }
+  const handleShowHint = () => {
+    setShowHint(!showHint);
+  }
+   const summary: Summary | null =
+        status === "match"
+            ? {
+                text: "🎉 All Correct! Great job",
+                color: "text-green-600",
+                bgColor: "bg-green-100",
+                borderColor: "border-green-600",
+            }
+            : status === "wrong"
+                ? {
+                    text: "❌ Some answers are wrong. Check again.",
+                    color: "text-red-600",
+                    bgColor: "bg-red-100",
+                    borderColor: "border-red-600",
+                }
+                : null
+
+
+
 
   return (
     <div className="w-full bg-[#fdeedc] rounded-xl p-6 shadow-md">
       {/* Passage */}
-      <p className="bg-[#e9543d] text-white rounded-lg p-4 mb-6">{description}</p>
+      <p className="bg-[#e9543d] text-white rounded-lg p-4 mb-6">
+        {description}
+      </p>
 
       {/* Question */}
       <h2 className="text-lg font-semibold mb-4">{question}</h2>
@@ -46,20 +87,26 @@ export default function ReadingFillBlanks({
       />
 
       {/* Feedback */}
-      {feedback && <p className="mb-2 font-medium">{feedback}</p>}
+      {/* {feedback && <p className="mb-2 font-medium">{feedback}</p>}
       {showHint && <p className="text-blue-600 mb-2">💡 Hint: {hint}</p>}
       {showSolution && (
         <p className="text-green-600 mb-2">
           ✅ Correct Answer: <b>{correctAnswer}</b>
         </p>
-      )}
+      )} */}
 
       {/* Buttons */}
-      <div className="flex gap-3 mt-4">
-        <Button onClick={checkAnswer} className="bg-[#dbeafe] hover:bg-[#dbeafe]/70 text-black border">
+      {/* <div className="flex gap-3 mt-4">
+        <Button
+          onClick={handleCheck}
+          className="bg-[#dbeafe] hover:bg-[#dbeafe]/70 text-black border"
+        >
           Check
         </Button>
-        <Button onClick={() => setShowHint(true)} className="bg-[#ffedd5] hover:bg-[#ffedd5]/70 text-black border">
+        <Button
+          onClick={() => setShowHint(true)}
+          className="bg-[#ffedd5] hover:bg-[#ffedd5]/70 text-black border"
+        >
           Hint
         </Button>
         <Button
@@ -68,7 +115,16 @@ export default function ReadingFillBlanks({
         >
           Show Solution
         </Button>
-      </div>
+      </div> */}
+
+        <Controllers
+              handleCheck={handleCheck}
+              handleShowSolution={handleShowSolution}
+              handleShowHint={handleShowHint}
+            />
+             {showHint && <Hint hint={hint} />}
+            <Check summary={summary} />
+
     </div>
   );
 }
