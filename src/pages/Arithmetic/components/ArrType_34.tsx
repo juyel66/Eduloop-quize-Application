d7dc9b29e95de894d9902cd9cb34d2dbe9f4b105
bodruddozaredoy <bodruddozaredoy@gmail.com>
2025-09-16 06:29:44 +0600

    feat: add new ArrType components (31-35) and update QuestionRenderer to support them; enhance QUESTIONS_DATA with new types

---
+++ src/pages/Arithmetic/components/ArrType_34.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ArrType_34() {
+  return (
+    <div>ArrType_34</div>
+  )
+}
---
+++ src/pages/Arithmetic/components/ArrType_35.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ArrType_35() {
+  return (
+    <div>ArrType_35</div>
+  )
+}
